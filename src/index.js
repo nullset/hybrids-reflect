9b9fc5196c47544e73130c1f07b16e7578072cb3
Nathan Wright <nathan@brandalism.com>
2020-05-03 13:10:39 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { property } from "hybrids";
-import { camelToDash } from "hybrids/src/utils";
-import { getType, coerceToType, setAttr } from "./utils";
+import { property } from 'hybrids';
+import { camelToDash } from 'hybrids/src/utils';
+import { getType, coerceToType, setAttr } from './utils';
 
 // Keep track of any instances of any components that use reflected attributes.
 const hosts = new WeakMap();
@@ -8,13 +8,79 @@ const hosts = new WeakMap();
 // Keep track of all reflected attributes, by component tag name.
 const reflectedAttributes = new Map();
 
+function createMutationObserver(host, tagName) {
+  const observer = new MutationObserver((mutations) => {
+    const watchedAttrs = reflectedAttributes.get(tagName);
+    mutations.forEach(({ attributeName, target }) => {
+      const watchedAttr = watchedAttrs.get(attributeName);
+      if (watchedAttr) {
+        const { key, type } = watchedAttr;
+        const attrValue = target.getAttribute(attributeName);
+        const reflectedValue = coerceToType(attrValue, type);
+        if (reflectedValue != undefined && reflectedValue !== host[key]) {
+          target[key] = reflectedValue;
+        }
+      }
+    });
+  });
+  hosts.set(host, true);
+  observer.observe(host, { attributes: true });
+  return observer;
+}
+
 export default function reflect(value, methods = {}) {
+  let type, attrName, observer;
+
+  function connectFn(host, key) {
+    console.log(key);
+    let disconnectFn;
+    type = methods.type || getType(value);
+    attrName = camelToDash(key);
+    const tagName = host.tagName;
+
+    // Assign all reflected attributes to a map whose lookup is the tagName.
+    const attrMap = reflectedAttributes.get(tagName) || new Map();
+    reflectedAttributes.set(tagName, attrMap.set(attrName, { key, type }));
+
+    // Set coerced value for key, as derived from attribute.
+    const attrValue = host.getAttribute(attrName);
+    if (attrValue === null) {
+      setAttr(host, attrName, type, value);
+    } else {
+      value = coerceToType(attrValue, type);
+    }
+    host[key] = value;
+
+    if (!hosts.get(host)) {
+      observer = createMutationObserver(host, tagName);
+      return (disconnectFn = () => {
+        alert('connectFn Disconnect');
+        observer.disconnect();
+        hosts.delete(host);
+      });
+    }
+  }
+
+  const properties = {
+    ...property(value, connectFn),
+  };
+  if (methods.connect) properties.connect = methods.connect;
+  if (methods.get) properties.get = methods.get;
+  if (methods.set) properties.set = methods.set;
+  if (methods.observe) properties.observe = methods.observe;
+
+  return properties;
+}
+
+export function reflect2(value, methods = {}) {
   let type;
   let attrName;
   let observer;
   let reflectedValue = value;
   const properties = {
     ...property(reflectedValue, function connect(host, key) {
+      console.log(key);
+      debugger;
       type = methods.type || getType(reflectedValue);
       attrName = camelToDash(key);
       const tagName = host.tagName;
@@ -56,6 +122,7 @@ export default function reflect(value, methods = {}) {
       if (methods.connect) {
         disconnectFn = methods.connect(host, key);
       }
+      debugger;
 
       // Once a host disconnects, stop watching it and remove it from WeakMap.
       // Only run code once no matter how many reflected keys it has.
