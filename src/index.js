e33816980f929966a148ce3e85b8b53378befaa7
Nathan Wright <nathan@brandalism.com>
2020-05-05 01:24:39 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { property } from 'hybrids';
 import { camelToDash } from 'hybrids/src/utils';
 import { getType, coerceToType, setAttr } from './utils';
+import fastdom from 'fastdom';
 
 // Keep track of any instances of any components that use reflected attributes.
 const hosts = new WeakMap();
@@ -29,9 +30,30 @@ function createMutationObserver(host, tagName) {
 }
 
 export default function reflect(value, methods = {}) {
+  // return {
+  //   get: (host, val = value) => val,
+  //   set: (host, val, oldValue) => transform(val, oldValue),
+  //   connect:
+  //     type !== "object" && type !== "undefined"
+  //       ? (host, key, invalidate) => {
+  //           if (host[key] === value) {
+  //             const attrName = camelToDash(key);
+
+  //             if (host.hasAttribute(attrName)) {
+  //               const attrValue = host.getAttribute(attrName);
+  //               host[key] =
+  //                 attrValue === "" && transform === Boolean ? true : attrValue;
+  //             }
+  //           }
+
+  //           return connect && connect(host, key, invalidate);
+  //         }
+  //       : connect,
+  // };
+
   let type, attrName, observer;
 
-  function connectFn(host, key) {
+  function observeFn(host, key) {
     console.log(key);
     let disconnectFn;
     type = methods.type || getType(value);
@@ -48,42 +70,87 @@ export default function reflect(value, methods = {}) {
       setAttr(host, attrName, type, value);
     } else {
       value = coerceToType(attrValue, type);
+      if (host[key] !== value) {
+        fastdom.mutate(() => {
+          host[key] = value;
+        });
+      }
     }
 
-    // If the property already has the same value, do nothing.
-    if (host[key] !== value) {
-      host[key] = value;
-    }
+    // // If the property already has the same value, do nothing.
+    // if (host[key] !== value) {
+    //   host[key] = value;
+    // }
 
     if (!hosts.get(host)) {
       observer = createMutationObserver(host, tagName);
-      return (disconnectFn = () => {
+      disconnectFn = () => {
         alert('connectFn Disconnect');
         observer.disconnect();
         hosts.delete(host);
-      });
+      };
     }
+
+    return {
+      value,
+      disconnectFn,
+    };
   }
 
-  const properties = {
-    ...property(value, connectFn),
+  let initialValue;
+  return {
+    connect: (host, key) => {
+      console.log('connect', key);
+      initialValue = observeFn(host, key);
+      const disconnectFn = methods.connect && methods.connect(host, key);
+      // host[key] = observer.value;
+      return () => {
+        if (disconnectFn) disconnectFn();
+        if (initialValue.disconnectFn) initialValue.disconnectFn();
+      };
+    },
+    get: (host, val, oldValue) => {
+      console.log('get');
+      const reflectedVal = methods.get && methods.get(host, val, oldValue);
+      return reflectedVal == undefined ? val : reflectedVal;
+    },
+    set: (host, val, oldValue) => {
+      console.log('set', attrName);
+      // debugger;
+      const reflectedVal = methods.set && methods.set(host, val, oldValue);
+      return reflectedVal === undefined ? type(val) : reflectedVal;
+    },
+    observe: (host, val, oldValue) => {
+      console.log('observe');
+      setAttr(host, attrName, type, val, oldValue);
+      if (methods.observe) methods.observe(host, val, oldValue);
+    },
   };
 
-  if (methods.connect) properties.connect = methods.connect;
+  const properties = {
+    ...property(value),
+  };
+
   if (methods.get) properties.get = methods.get;
   if (methods.set) properties.set = methods.set;
+  if (methods.connect) properties.connect = methods.connect;
+
   // properties.set = (host, value, oldValue) => {
   //   const v =   methods.set ? methods.set(host, value, oldValue) : properties.set(host, value, oldValue);
   //   debugger;
   //   return v;
   // }
   // if (methods.observe) properties.observe = methods.observe;
-  properties.observe = (host, val, oldValue) => {
-    setAttr(host, attrName, type, val, oldValue);
-    if (methods.observe) methods.observe(host, val, oldValue);
-  };
+  // properties.observe = (host, val, oldValue) => {
+  //   setAttr(host, attrName, type, val, oldValue);
+  //   if (methods.observe) methods.observe(host, val, oldValue);
+  // }
 
-  return properties;
+  return {
+    connect: properties.connect,
+    set: properties.set,
+    get: properties.get,
+  };
 }
 
 // export   function reflect2(value, methods = {}) {
